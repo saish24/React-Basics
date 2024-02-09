0d89030c4295af2a379b712313e3bd744228c9dd
saish24 <saish112@gmail.com>
2024-02-09 08:44:54 +0530

    modifying method

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,38 +1,46 @@
 import React, { Component } from "react";
 
 class Message extends Component {
-    constructor() {
-        super()
-        this.state = {
-            'subscribed': false,
-            'message': "Welcome to this website!",
-            'subscribeMessage': "Click to subscribe"
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      subscribed: false,
+      message: "Welcome to this website!",
+      subscribeMessage: "Click to subscribe",
+    };
+  }
 
-    OnClickSubscribe() {
-        var message = "Welcome to this website!"
-        var subscribeMessage = "Click to subscribe"
-        if (this.state.subscribed === true) {
-            message="Thank you for subscribing!"
-            subscribeMessage="Subscribed ✅"
-        }
+  OnClickSubscribe() {
+    this.setState((prevState, props) => {
+      var message = "Welcome to this website!";
+      var subscribeMessage = "Click to subscribe";
 
-        this.setState({
-            subscribed: !this.state.subscribed,
-            message: message,
-            subscribeMessage: subscribeMessage
-        })
-    }
+      if (prevState.subscribed === true) {
+        message = "Thank you for subscribing!";
+        subscribeMessage = "Subscribed ✅";
+      }
+      return {
+        subscribed: !prevState.subscribed,
+        message: message,
+        subscribeMessage: subscribeMessage,
+      };
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>{this.state.message}</h1>
-                <button onClick={()=>{this.OnClickSubscribe()}}>{this.state.subscribeMessage}</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.message}</h1>
+        <button
+          onClick={() => {
+            this.OnClickSubscribe();
+          }}
+        >
+          {this.state.subscribeMessage}
+        </button>
+      </div>
+    );
+  }
 }
 
-export default Message
+export default Message;
